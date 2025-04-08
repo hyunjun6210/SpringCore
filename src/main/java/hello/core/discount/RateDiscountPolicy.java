fe7bd13eb0d88b7182b8cb260edb1e71d360b1d3
hyunjun.jin <hyunjun6210@naver.com>
2025-04-08 18:18:42 +0900

    Annotation 정의

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Component //Component scan 하려면 구현체에 붙여야함
 //@Qualifier("mainDiscountPolicy")
-@Primary
+//@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
-
     private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
