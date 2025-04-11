b3f09e07dbf8caee0f2d695aa82790870b48b1ea
hyunjun.jin <hyunjun6210@naver.com>
2025-04-11 15:35:27 +0900

    provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -33,36 +34,19 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; //생성시점에 주입 x01
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
-    }
-
-    @Scope("singleton")
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean; //생성시점에 주입 x02
-
-        @Autowired
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
     }
 
